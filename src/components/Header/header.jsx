080c8d60c01e6c8f0efc974596f5c65e5095201a
Quvonchbek3 <qnomozov3@gmail.com>
2024-06-17 08:00:50 +0500

    main

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -12,14 +12,14 @@ function Headers() {
       <Header />
       <div className="flex justify-between">
       <div className={`${darkMode ? "header" : "headres"} h-[300px] sm:h-[600px]`}>
-      <div className="flex justify-center pr-[80px] pt-[4%]">
+      <div className="flex justify-between px-0 gap-32 sm:px-[120px] pt-[4%]">
         <div className="w-[40%]" data-aos="fade-right">
           <h1 className="text-blue-600 ml-7 sm:ml-0 dark:text-gray-200 font-[700] text-[100%] sm:text-[200%] md:text-[300%]">
             Xalqaro&nbsp;darajadagi&nbsp;IT<br /> companiyaga <br /> xush kelibsizlar
           </h1>
           <p className="text-gray-500 dark:text-gray-400 font-[400] text-[65%] ml-7 sm:ml-0 sm:text-[100%]">Sizni ko’rib turganimizdan <br /> juda ham xursandmiz!</p>
         </div>
-        <div className="flex w-[full] sm:w-[35%] items-center"  data-aos="fade-left">
+        <div className="flex w-[full] sm:w-[40%] items-center"  data-aos="fade-left">
         <div className="sm:w-[100%]">
             <img src="images/img_img_boy.png" className="w-full" alt="logo" />
         </div>
